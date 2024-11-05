015cc4f8b0edb4349805b3edfeca427541cf024c
m Arslan <arxlanale302@gmail.com>
2024-11-05 13:50:27 -0800

    creat navbar component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
    <>
-   <h1 className="text-gray-600 text-9xl text-center">Hello World</h1>
+   <h1 className="text-gray-600 text-9xl text-center">hy this is arslan</h1>
    </>
   );
 }
