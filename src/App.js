db3b8fa8961cb6211359472149baec69b7c87218
Afzaal <muhammadafzaalgujjar52@gamil.com>
2024-11-06 02:21:58 +0500

    Text changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 function App() {
   return (
    <>
-   <h1 className="text-gray-600 text-9xl text-center">Hello World</h1>
+   <h1 className="text-gray-600 text-9xl text-center">Hey this is Afzaal
+   </h1>
    </>
   );
 }
